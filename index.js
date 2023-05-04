01a9b13910f5b52e72a1f1c2e25c527f2c2c69ca
AmdjedBelgacem <amgedbelgacem@gmail.com>
2023-05-04 23:42:29 +0300

    Done

--- index.js
+++ index.js
@@ -1,1 +1,10 @@
 // code your solution here
+
+function superbowlWin(arg) {
+  let win = arg.find(obj => obj.result === "W");
+  if (win) {
+    return win.year;
+  } else {
+    return undefined;
+  }
+}
